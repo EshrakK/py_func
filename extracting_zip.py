0a071b97436333f2ab6b8aef347adf362dd1ddd2
Eshrak Kader <79409488+EshrakK@users.noreply.github.com>
2021-03-04 20:02:05 +0600

    Delete extracting_zip.py

--- extracting_zip.py
+++
@@ -1,9 +0,0 @@
-#Extracting all files from ZIP file
-import zipfile
-
-#make sure to keep the python file and .zip file in same directory or provide the absolute path.
-MyFolder = zipfile.ZipFile('MyFolder.zip')
-
-#provide the path of the directory for extracted folder
-MyFolder.extractall('C:/Users/Eshrak/Programming/GitHub Repo/py_func/')
-MyFolder.close()
