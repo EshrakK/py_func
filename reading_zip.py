59e0661ff1f8eb36b14c7641a70f47af3c645059
Eshrak Kader <79409488+EshrakK@users.noreply.github.com>
2021-03-04 20:02:18 +0600

    Delete reading_zip.py

--- reading_zip.py
+++
@@ -1,7 +0,0 @@
-#Reading contents of ZIP file
-import zipfile
-
-#make sure to keep the python file and .zip file in same directory or provide the absolute path.
-MyFolderZip = zipfile.ZipFile('MyFolder.zip')
-f = MyFolderZip.namelist()
-print(f)
